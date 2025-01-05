5085ea82f9dc7ebbd6e18e9fbb0886069961d931
fancilivain <meigangwen@gmail.com>
2025-01-05 21:29:50 +0800

    Before creating the lambda function

--- sqs_lambda_demo/sqs_lambda_demo_stack.py
+++ sqs_lambda_demo/sqs_lambda_demo_stack.py
@@ -2,10 +2,9 @@ from constructs import Construct
 from aws_cdk import (
     Duration,
     Stack,
-    aws_iam as iam,
     aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_lambda as lambda_,
+    aws_lambda_event_sources as lambda_event_sources
 )
 
 
@@ -14,13 +13,13 @@ class SqsLambdaDemoStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        # Create our Queue
         queue = sqs.Queue(
             self, "SqsLambdaDemoQueue",
             visibility_timeout=Duration.seconds(300),
         )
 
-        topic = sns.Topic(
-            self, "SqsLambdaDemoTopic"
-        )
+        # Create our lambda Function
+        
 
-        topic.add_subscription(subs.SqsSubscription(queue))
+        
